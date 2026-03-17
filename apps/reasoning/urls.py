9c2f997947d583b4fc8193d2c5bfc32210c7f198
Manish Kafle <Manishkafle03@gmail.com>
2026-03-17 19:27:37 +0545

    Refactor: Switched to Django-Ninja; removed apps's urls.py in favor of API

--- apps/reasoning/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('reason/', views.analyze_threat_details, name='reason_threat'),
-]
--- apps/response/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('execute/', views.decide_response, name='execute_response'),
-]
